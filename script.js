3625010dbf67e221078d88c31f1ad772d88eef10
antek-bizon <antek.bizon@gmail.com>
2024-03-21 17:14:47 +0100

    style update

--- script.js
+++ script.js
@@ -14,65 +14,65 @@ const bingoItems = [
   'Żart ze Śmietany',
   'Żart z Dawida',
   'Opowieści z innych klas',
-  '\"Schowanie telefonów\"',
-  '\"Prawda Paweł?\"',
-  '\"Chodź tu do pani\"',
-  '\"Czy jesz na oczach wygłodniałego pracownika budżetówki?\"',
-  '\"Kocham miłością wielką\"',
+  '"Schowanie telefonów"',
+  '"Prawda Paweł?"',
+  '"Chodź tu do pani"',
+  '"Czy jesz na oczach wygłodniałego pracownika budżetówki?"',
+  '"Kocham miłością wielką"',
   'Wełpa z mokrymi włosami',
   'Historia o synu',
   'Gadanie o słodyczach/jedzeniu',
   'Żart seksistowski',
   'Paweł mówi za cicho',
-  '\"Pomyśleć/przyjąć na zasadzie dogmatu\"',
+  '"Pomyśleć/przyjąć na zasadzie dogmatu"',
   'Michał ucieka z lekcji',
   'Nazywanie kogoś dzieckiem',
   'Ploteczki',
   'Narzekanie na niską frekwencję',
   'Zapowiedź sprawdzianu bez powtórki',
   'Dymy w gabinecie polonistów',
-  '\"Moje [...] serce (np. polonistyczne)\"',
-  '\"Dzień dobry panowie\" + trzaśnięcie drzwiami'
+  '"Moje [...] serce (np. polonistyczne)"',
+  '"Dzień dobry panowie" + trzaśnięcie drzwiami',
 ]
 const width = 5
 const height = width
 const center = Math.floor(width / 2)
-const main = document.querySelector("main")
+const main = document.querySelector('main')
 
 generateTableFromCookies()
 
 function shuffle(toShuffle) {
   const array = [...toShuffle]
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
   return array
 }
 
 function onBingoSelect(cell) {
-  if (cell.classList.contains("selected")) {
-    cell.classList.remove("selected")
+  if (cell.classList.contains('selected')) {
+    cell.classList.remove('selected')
   } else {
-    cell.classList.add("selected")
+    cell.classList.add('selected')
   }
   updateCookies()
 }
 
 function genereteTable(items) {
   let i = 0
-  const table = document.createElement("table")
-  table.id = "bingo-table"
-  
+  const table = document.createElement('table')
+  table.id = 'bingo-table'
+
   for (let y = 0; y < height; y++) {
-    const row = document.createElement("tr")
+    const row = document.createElement('tr')
     for (let x = 0; x < width; x++) {
-      const cell = document.createElement("td")
+      const cell = document.createElement('td')
       if (x === center && y === center) {
-        cell.innerText = "X"
-        cell.classList.add("center")
+        cell.innerText = 'X'
+        cell.classList.add('center')
       } else {
         cell.onclick = () => onBingoSelect(cell)
         cell.innerText = items[i]
@@ -88,7 +88,7 @@ function genereteTable(items) {
 
 function getCookies() {
   try {
-    const cookieString = document.cookie.split("json=")[1] 
+    const cookieString = document.cookie.split('json=')[1]
     return JSON.parse(cookieString)
   } catch (e) {
     return null
@@ -100,21 +100,21 @@ function generateTableFromCookies() {
   if (!cookieJson) return
 
   let i = 0
-  const table = document.createElement("table")
-  table.id = "bingo-table"
-  
+  const table = document.createElement('table')
+  table.id = 'bingo-table'
+
   for (let y = 0; y < height; y++) {
-    const row = document.createElement("tr")
+    const row = document.createElement('tr')
     for (let x = 0; x < width; x++) {
-      const cell = document.createElement("td")
+      const cell = document.createElement('td')
       if (x === center && y === center) {
-        cell.innerText = "X"
-        cell.classList.add("center")
+        cell.innerText = 'X'
+        cell.classList.add('center')
       } else {
         cell.onclick = () => onBingoSelect(cell)
         cell.innerText = cookieJson[i].text
         if (cookieJson[i].isSelected) {
-          cell.classList.add("selected")
+          cell.classList.add('selected')
         }
       }
       i++
@@ -127,11 +127,11 @@ function generateTableFromCookies() {
 }
 
 function updateCookies() {
-  const cells = document.querySelectorAll("#bingo-table td")
-  if (!cells || cells.length === 0) return 
+  const cells = document.querySelectorAll('#bingo-table td')
+  if (!cells || cells.length === 0) return
 
   const date = new Date()
-  date.setDate(date.getDate() + 1);
+  date.setDate(date.getDate() + 1)
   date.setHours(1)
   date.setMinutes(0)
   date.setSeconds(0)
@@ -139,28 +139,33 @@ function updateCookies() {
   const items = new Array(cells.length)
   let i = 0
   for (const cell of cells) {
-    items[i] = {text: cell.textContent, isSelected: cell.classList.contains("selected")}
+    items[i] = {
+      text: cell.textContent,
+      isSelected: cell.classList.contains('selected'),
+    }
     i++
   }
 
-  document.cookie = `json=${JSON.stringify(items)}; expires=${date.toUTCString()};path=/;SameSite=Strict`
+  document.cookie = `json=${JSON.stringify(
+    items,
+  )}; expires=${date.toUTCString()};path=/;SameSite=Strict`
 }
 
-document.getElementById("gen-btn").onclick = () => {
-  main.innerHTML = ""
+document.getElementById('gen-btn').onclick = () => {
+  main.innerHTML = ''
 
   const bingoTable = genereteTable(shuffle(bingoItems))
 
-  main.append(bingoTable)  
+  main.append(bingoTable)
   updateCookies()
 }
 
-document.getElementById("reset-btn").onclick = () => {
-  const cells = document.querySelectorAll("#bingo-table td")
+document.getElementById('reset-btn').onclick = () => {
+  const cells = document.querySelectorAll('#bingo-table td')
   if (!cells || cells.length === 0) return
 
   for (const cell of cells) {
-    cell.classList.remove("selected")
+    cell.classList.remove('selected')
   }
 
   updateCookies()
